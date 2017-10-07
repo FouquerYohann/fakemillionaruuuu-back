ed85cbc5b2e97cd83b1ad6362a39ec7ac6cbfbb6
YFouquer <yohann.fouquer@lesfurets.com>
2017-10-07 21:59:42 +0200

    :construction: trying to deploy

--- src/main/com/servlets/ShowUsers.java
+++ src/main/com/servlets/ShowUsers.java
@@ -4,10 +4,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
+import java.sql.*;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.*;
 
+import main.com.stuff.DBUtils;
 
 public class ShowUsers extends HttpServlet {
 
@@ -16,30 +18,61 @@ public class ShowUsers extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
         resp.setContentType("text/html");
         ServletOutputStream outputStream = resp.getOutputStream();
 
         outputStream.print("YOOO");
         req.getRequestDispatcher("/index.jsp").forward(req, resp);
 
-//                try {
-//                    Connection connexion = DBUtils.getConnexion();
-//                    Statement statement = connexion.createStatement();
-//                    ResultSet resultSet = statement.executeQuery("SELECT * FROM Users;");
-//                    outputStream.println("<table>");
-//                    while (resultSet.next()) {
-//                        outputStream.println("<tr>");
-//                        outputStream.println("<td>" + resultSet.getString("PersonID") + "</td");
-//                        outputStream.println("<td>" + resultSet.getString("Login") + "</td");
-//                        outputStream.println("<td>" + resultSet.getString("Password") + "</td");
-//                        outputStream.println("</tr>");
-//                    }
-//                    outputStream.println("</table>");
-//
-//                    req.getRequestDispatcher("/index.jsp").forward(req, resp);
-//                } catch (SQLException e) {
-//                    outputStream.print(System.getenv("JDBC_DATABASE_URL"));
-//                    outputStream.print(e.getMessage());
-//                }
+        try {
+            Connection connexion = DBUtils.getConnexion();
+            Statement statement = connexion.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Users;");
+            outputStream.println("<table>");
+            while (resultSet.next()) {
+                outputStream.println("<tr>");
+                outputStream.println("<td>" + resultSet.getString("PersonID") + "</td");
+                outputStream.println("<td>" + resultSet.getString("Login") + "</td");
+                outputStream.println("<td>" + resultSet.getString("Password") + "</td");
+                outputStream.println("</tr>");
+            }
+            outputStream.println("</table>");
+
+            req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            outputStream.print(System.getenv("JDBC_DATABASE_URL"));
+            outputStream.print(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        resp.setContentType("text/html");
+        ServletOutputStream outputStream = resp.getOutputStream();
+
+        outputStream.print("YOOO");
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+
+        try {
+            Connection connexion = DBUtils.getConnexion();
+            Statement statement = connexion.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Users;");
+            outputStream.println("<table>");
+            while (resultSet.next()) {
+                outputStream.println("<tr>");
+                outputStream.println("<td>" + resultSet.getString("PersonID") + "</td");
+                outputStream.println("<td>" + resultSet.getString("Login") + "</td");
+                outputStream.println("<td>" + resultSet.getString("Password") + "</td");
+                outputStream.println("</tr>");
+            }
+            outputStream.println("</table>");
+
+            req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            outputStream.print(System.getenv("JDBC_DATABASE_URL"));
+            outputStream.print(e.getMessage());
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,24 +1,8 @@
-<%@ page import="main.com.stuff.DBUtils" %>
-<%@ page import="java.sql.*" %>
 <html>
 <body>
 <h2>Hello World!</h2>
 </body>
 <%
     out.print(System.getenv("JDBC_DATABASE_URL"));
-%>
-<%
-    Connection connexion = DBUtils.getConnexion();
-    Statement statement = connexion.createStatement();
-    ResultSet resultSet = statement.executeQuery("SELECT * FROM Users;");
-    out.println("<table>");
-    while (resultSet.next()) {
-        out.println("<tr>");
-        out.println("<td>" + resultSet.getString("PersonID") + "</td");
-        out.println("<td>" + resultSet.getString("Login") + "</td");
-        out.println("<td>" + resultSet.getString("Password") + "</td");
-        out.println("</tr>");
-    }
-    out.println("</table>");
 %>
 </html>
