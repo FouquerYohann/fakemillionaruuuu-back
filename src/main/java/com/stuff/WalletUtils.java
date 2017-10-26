c90a3833299711a4aff27767bd565169c45bb67d
YFouquer <yohann.fouquer@lesfurets.com>
2017-10-26 17:51:51 +0200

    :construction: Orders push fin journee

--- src/main/java/com/stuff/WalletUtils.java
+++ src/main/java/com/stuff/WalletUtils.java
@@ -1,16 +1,15 @@
 package com.stuff;
 
-import com.stuff.PoloAPI.CURRENCIES;
-import org.json.JSONObject;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import static com.stuff.DBUtils.DATE_TIME_FORMATTER;
 import static com.stuff.DBUtils.getConnexion;
 import static java.time.LocalDateTime.now;
+
+import java.sql.*;
+import java.util.UUID;
+
+import org.json.JSONObject;
+
+import com.stuff.PoloAPI.CURRENCIES;
 
 /**
  * Created by Yohann on 26/10/2017.
@@ -22,14 +21,16 @@ public class WalletUtils {
 
         try {
             PreparedStatement insert = connection
-                    .prepareStatement("INSERT INTO offres (personid,buy,currency,quantity,price,time_start) " +
-                            "        VALUES (?, ?, ?, ?, ?, ?);");
+                            .prepareStatement("INSERT INTO offres (personid,buy,currency,quantity,price,time_start," +
+                                            "offer_uuid) " +
+                                            "        VALUES (?, ?, ?, ?, ?, ?, ?);");
             insert.setInt(1, id);
             insert.setBoolean(2, buy);
             insert.setString(3, currency.toString());
             insert.setDouble(4, quantity);
             insert.setDouble(5, price);
             insert.setString(6, DATE_TIME_FORMATTER.format(now()));
+            insert.setString(7, String.valueOf(UUID.randomUUID()));
 
             if (insert.executeUpdate() == 1) {
                 return new JSONObject().put("err", 200);
@@ -42,32 +43,60 @@ public class WalletUtils {
         return new JSONObject().put("err", 417);
     }
 
-
-    public static JSONObject checkForSales(int id, boolean buy, CURRENCIES currencies, double quantity, double price) {
+    public static JSONObject checkForSales(int id, boolean buy, CURRENCIES currencies, double myQuantity, double myPrice) {
         Connection connection = getConnexion();
 
         try {
             PreparedStatement query = connection
-                    .prepareStatement("SELECT * FROM offres where currency = ? AND price <= ? AND buy = ?");
+                            .prepareStatement("SELECT * FROM offres where currency = ? AND price <= ? AND buy = ?");
             query.setString(1, currencies.toString());
-            query.setDouble(2, price);
+            query.setDouble(2, myPrice);
             query.setBoolean(3, !buy);
-
 
             ResultSet resultSet = query.executeQuery();
 
-            while (resultSet.next() && quantity >0) {
+            while (resultSet.next() && myQuantity > 0) {
                 double otherQuantity = resultSet.getDouble("quantity");
-                if (quantity < otherQuantity) {
+                double otherPrice = resultSet.getDouble("price");
+                double otherTotalPrice = otherQuantity * otherPrice;
+                double myTotalPrice = myPrice * myQuantity;
+                String offer_uuid;
+                if (myTotalPrice < otherTotalPrice) {
+                    double quantityBought = myTotalPrice / otherPrice;
 
+                    offer_uuid = resultSet.getString("offer_UUID");
+                    PreparedStatement upd = connection
+                                    .prepareStatement("UPDATE offres SET quantity = offres.quantity + ? WHERE " +
+                                                    "offer_uuid = ?;");
+                    upd.setDouble(1, -quantityBought);
+                    upd.setString(2, offer_uuid.toString());
+                    upd.executeUpdate();
+                    //TODO
+                    WalletUtils.moveOrderToLogs();
+                } else {
+
+
+                    myQuantity -= otherQuantity;
+                    myTotalPrice -=otherTotalPrice;
+                    //TODO
+                    WalletUtils.moveOrderToLogs();
                 }
-
             }
+            if (myQuantity > 0) {
+                postOrder(id, buy, currencies, myQuantity, myPrice);
+            } else {
+                WalletUtils.moveOrderToLogs();
+            }
+            return new JSONObject().put("err", 200);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return new JSONObject().put("err", 417);
+    }
+
+    private static void moveOrderToLogs() {
+
     }
 
 }
--- src/main/java/com/tests/MainTrifouille.java
+++ src/main/java/com/tests/MainTrifouille.java
@@ -1,19 +1,18 @@
 package com.tests;
 
+import static com.stuff.PoloAPI.CURRENCIES.BCH;
+
 import java.io.FileNotFoundException;
 import java.time.format.DateTimeFormatter;
 
 import com.stuff.DBUtils;
-import com.stuff.PoloAPI;
 import com.stuff.WalletUtils;
-
-import static com.stuff.PoloAPI.CURRENCIES.ETH;
 
 public class MainTrifouille {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) throws FileNotFoundException {
         DBUtils.addSession(1);
-        System.out.println(WalletUtils.postOrder(1,true, ETH,1.23,0.0565));
+        System.out.println(WalletUtils.postOrder(2,true, BCH,1.23,0.0432));
     }
 }
