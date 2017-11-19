be6a7bf61a53168d6924de35e8609b04fcbd5dac
FouquerYohann <yohann.fouquer@gmail.com>
2017-11-19 16:23:51 +0100

    :bug: addOrder

--- src/main/java/com/example/AddOrder.java
+++ src/main/java/com/example/AddOrder.java
@@ -4,6 +4,7 @@
 package com.example;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -21,6 +22,13 @@ public class AddOrder extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
+
+
+        Enumeration<String> params = req.getParameterNames();
+        while(params.hasMoreElements()){
+            String paramName = params.nextElement();
+            System.out.println("Parameter Name - "+paramName+", Value - "+req.getParameter(paramName));
+        }
 
         int id = Integer.parseInt(req.getParameter("id"));
         boolean buy = Boolean.parseBoolean(req.getParameter("buy"));
