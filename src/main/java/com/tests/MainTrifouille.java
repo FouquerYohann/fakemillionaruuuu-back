10d29e0e8ce8ac9b056aff552a2f5a4d4c9b528a
YFouquer <yohann.fouquer@lesfurets.com>
2017-11-21 18:10:53 +0100

    :construction: trade history

--- src/main/java/com/tests/MainTrifouille.java
+++ src/main/java/com/tests/MainTrifouille.java
@@ -3,12 +3,9 @@ package com.tests;
 import java.io.FileNotFoundException;
 import java.time.format.DateTimeFormatter;
 
-import com.stuff.DBUtils;
-
 public class MainTrifouille {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(DBUtils.inscription("cocolasticot","password"));
     }
 }
